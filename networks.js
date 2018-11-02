421acdd88d93cd0e7bd27096a723018a6a5c4f17
Dylan Seago <dylan.seago@gmail.com>
2018-11-02 15:44:30 -0400

    Use legacy bitcoin bip32 prefixes for back compat

--- networks.js
+++ networks.js
@@ -1,10 +1,12 @@
 // Source: https://github.com/cryptocoinjs/coininfo
+// Note: Using bitcoin legacy bip32 constants for backwards compatability.
+// We do this so that everything can work with xpub/xprv prefixed keys
 module.exports = {
   mainnet: {
     messagePrefix: '\u0019Litecoin Signed Message:\n',
     bip32: {
-      public: 0x019da462,
-      private: 0x019d9cfe
+      public: 0x0488b21e,
+      private: 0x0488ade4
     },
     pubKeyHash: 0x30,
     scriptHash: 0x32,
@@ -13,8 +15,8 @@ module.exports = {
   testnet: {
     messagePrefix: '\u0019Litecoin Signed Message:\n',
     bip32: {
-      public: 0x0436f6e1,
-      private: 0x0436ef7d
+      public: 0x043587cf,
+      private: 0x04358394
     },
     pubKeyHash: 0x6f,
     scriptHash: 0x3a,
