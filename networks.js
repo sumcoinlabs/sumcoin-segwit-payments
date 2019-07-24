800c81c8d9efe92855ea83c8ccdffc3b54b955d0
Sumcoin <37975862+sumcoinlabs@users.noreply.github.com>
2019-07-24 01:30:35 -0600

    marked todo's

--- networks.js
+++ networks.js
@@ -5,21 +5,21 @@ module.exports = {
   mainnet: {
     messagePrefix: '\u0019Sumcoin Signed Message:\n',
     bip32: {
-      public: 0x0488b41c,
+      public: 0x0488b41c, //complete
       private: 0x0488abe6 //complete
     },
-    pubKeyHash: 0x30, //todo's < and below
-    scriptHash: 0x32,
-    wif: 0xb0
+    pubKeyHash: 0x30, //todo
+    scriptHash: 0x32, //todo
+    wif: 0xb0 //todo
   },
   testnet: {
     messagePrefix: '\u0019Sumcoin Signed Message:\n',
     bip32: {
-      public: 0x0488b41c,
-      private: 0x0488abe6
+      public: 0x0488b41c, //complete
+      private: 0x0488abe6 //complete
     },
-    pubKeyHash: 0x6f,
-    scriptHash: 0x3a,
-    wif: 0xef
+    pubKeyHash: 0x6f, //todo
+    scriptHash: 0x3a, //todo
+    wif: 0xef //todo
   }
 }
